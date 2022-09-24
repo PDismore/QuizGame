35635177b126d29734ab73f9530b11a4116b11bc
PDismore <pat.dismore@gmail.com>
2022-09-24 17:04:22 -0400

    question array creation, new function testing

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 An app to quiz users on coding basics!
 
 ## Description 
-Using bootstrap and JS, this app will quiz users on coding basics, log their high scores and time them. 
+Using CSS and JS, this app will quiz users on coding basics, log their high scores and time them. 
 
 ## Installation
-No need for installation, simply go to _____
+No need for installation, simply go to the [webpage](https://pdismore.github.io/QuizGame/)
 
 ## Usage
 To use, simply click the start game button and the user will be prompted to answer questions until they either complete the quiz or time runs out
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,25 @@
 var startButton = document.getElementById('start-button');
 var startWindow = document.getElementById('start-window');
-var quizContents = document.getElementById('quiz-content')
-var questionContainer = document.getElementById('question-window')
+var quizContents = document.getElementById('quiz-content');
+var questionContainer = document.getElementById('question-window');
+var questions = [
+    {
+        question: "Example question",
+        answers: {
+            a: 'ex1',
+            b: 'ex2',
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "Example question",
+        answers: {
+            a: 'ex1',
+            b: 'ex2',
+        },
+        correctAnswer: 'a'
+    }
+];
 
 startButton.addEventListener('click', startGame);
 
@@ -10,17 +28,36 @@ startButton.addEventListener('click', startGame);
 function startGame(){
 
     startWindow.classList.add('hide');
-    quizContents, questionContainer.classList.add('show');
-    console.log(questionContainer)
+
+    function showQuestions(questions, questionContainer){
+        var output = [];
+        var answers;
+        for(var i=0; i<questions.length; i++){
+            answers = [];
+            for(letter in questions[i].answers){
+                answers.push(
+                    '<label>'
+                    + '<input type="radio" name="question'+i+'"value="'+letter+'">'
+                    + letter + ': '
+                    + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+            output.push(
+                '<div class="question">'+questions[i].question+'</div>'
+                +'<div class="answers">'+answers.join('')+'</div>'
+            );
+        }
+        questionContainer.innerHtml= output.join('');
+    }
+
+    showQuestions(questions, questionContainer);
+
+
+
+    // quizContents, questionContainer.classList.add('show');
+    // console.log(questionContainer)
     console.log('started');
 
 
-}
-
-function nextQuestion(){
-
-}
-
-function selectAnswer(){
-
-}
+};
