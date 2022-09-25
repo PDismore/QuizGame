57fa01748409749a0095eb2657acbc0e46cbd9c5
PDismore <pat.dismore@gmail.com>
2022-09-25 01:47:33 -0400

    trying new way, couldnt get next question to work

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
-var startButton = document.getElementById('start-button');
-var startWindow = document.getElementById('start-window');
-var quizContents = document.getElementById('quiz-content');
-var questionContainer = document.getElementById('question-window');
+const startButton = document.getElementById('start-button');
+const startWindow = document.getElementById('start-window');
+const quizContents = document.getElementById('quiz-content');
+const questionContainer = document.getElementById('question-window');
 const currentScore = document.getElementById('score');
-const nextBtn = document.getElementById('next')
-var currentQuestion = 0;
+const questionElement = document.getElementById('title');
+const answerElement = document.getElementById('next')
+let shuffledQuestions, currentQuestionIndex
 var score = 0;
 
-nextBtn.addEventListener("click", next)
-var questionArray = [
+// Array storing the questions for the quiz and their answers
+const questionArray = [
     
     {
         question: "Example question",
@@ -25,13 +26,11 @@ var questionArray = [
 
 ];
 
-
-
-
 startButton.addEventListener('click', startGame);
 
 // hides the start window and shows the quiz window
 function startGame(){
+    // Timer
     var timeLeft = 60;
     var timer = setInterval(function(){
         timeLeft--;
@@ -39,49 +38,69 @@ function startGame(){
         if(timeLeft <=0)
             clearInterval(timer);
     },1000);
+    // Hides the start window and shows the question window
     startWindow.classList.add('hide');
     quizContents.classList.remove('hide');
     console.log('started');
-
-
-
-    currentQuestion =0;
-
-
-    // // Function that will cycle through the questions in questionArray variable
-    function showQuestions(q){
-
-        // shows the question from the array
-        var titleDiv = document.getElementById('title');
-        titleDiv.textContent= q[0].question;
-        
-        // shows the answers for the question from array
-        var ans = document.querySelectorAll('.answers');
-        // console.log(ans);
-        console.log(q);
-        ans.forEach(function(element, index){
-            element.textContent = q[0].answers[index];
-            element.addEventListener('click', function(){
-                if(q[q.length-1].correctAnswer==index){
-                    console.log('correct answer');
-                    score++;
-                    console.log(score);
-                    currentScore.innerHTML=score;
-                } else{
-                    score--;
-                    console.log('wrong answer');
-                    console.log(score);
-                    currentScore.innerHTML= score;
-                }
-            }
-            );
-        });
-    }
-    showQuestions(questionArray);
-
-
+    currentQuestionIndex = 0;
+    
 
 };
+
+function setNextQuestion (){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+    questionElement.textContent=question.question
+
+}
+
+console.log(questionElement)
+
+
+
+
+
+
+// Function that will cycle through the questions in questionArray variable
+// function showQuestions(q){
+
+//      // shows the question from the array
+//      var titleDiv = document.getElementById('title');
+//      titleDiv.textContent= q[0].question;
+        
+//     // shows the answers for the question from array
+//     var ans = document.querySelectorAll('.answers');
+//      // console.log(ans);
+//      console.log(q);
+//      ans.forEach(function(element, index){
+//          //sets the text from array
+//          element.textContent = q[0].answers[index];
+//          element.addEventListener('click', function(){
+
+//             // Adds 1 to the score for a correct answer
+//              if(q[q.length-1].correctAnswer==index){
+//                  console.log('correct answer');
+//                  score++;
+//                  console.log(score);
+//                 currentScore.innerHTML=score;
+
+//                 //Subtracts 1 from the score on a wrong answer
+//             } else{
+//                  score--;
+//                  console.log('wrong answer');
+//                 console.log(score);
+//                 currentScore.innerHTML= score;
+//              }
+//         }
+//     );
+//         });
+//     };
+//     showQuestions(questionArray);
+
+
+
 console.log(score);
 
 
